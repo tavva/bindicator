e15f7cfc92b0d0d3066e6679a218e855246c2f4c
Ben Phillips <ben@ben-phillips.net>
2025-01-14 16:05:22 +0000

    We'll need some secrets

---
+++ secrets.h
@@ -0,0 +1,13 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#include <Arduino.h>
+
+extern const char* WIFI_SSID;
+extern const char* WIFI_PASSWORD;
+
+extern const String CLIENT_ID;
+extern const String CLIENT_SECRET;
+extern const String REFRESH_TOKEN;
+
+#endif
